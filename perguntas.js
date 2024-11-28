141d13e2732fefb6b0ef1e6274e0dc47d16028a8
annacirino <annaflaviaestrela@gmail.com>
2024-11-28 17:14:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Acre?',
+    'A capital do Acre é o Rio Branco'
+)
+
+criaCartao(
+    'Série',
+    'Qual é a melhor série para assistir?',
+    'Gray"s Anatomy'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amigo em Inglês?',
+    'amigo em ingles é Friend (RAI)'
+)
